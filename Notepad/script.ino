d07a9f83930537b8e4708242256164bd2158b494
Saif Islam <saifislam23122005@gmail.com>
2023-05-27 20:57:19 +0600

    Update script.ino

--- Notepad/script.ino
+++ Notepad/script.ino
@@ -1,33 +1,33 @@
 #include "DigiKeyboard.h"
 
 void setup() {
-pinMode(1, OUTPUT);
+  pinMode(1, OUTPUT);
 }
 
 void loop() {
-// Open Notepad by pressing the Windows key and typing "notepad"
-DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-DigiKeyboard.delay(300);
-DigiKeyboard.print("notepad");
-DigiKeyboard.sendKeyStroke(KEY_ENTER);
-DigiKeyboard.delay(1000);
+  // Open Notepad by pressing the Windows key and typing "notepad"
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("notepad");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
 
-// Type "I love you" 100 times
-for (int i = 0; i < 100; i++) {
-DigiKeyboard.print("I love you");
-DigiKeyboard.sendKeyStroke(13); // Use the ASCII code for Enter key
-DigiKeyboard.delay(100);
-}
+  // Type "I love you" 100 times
+  for (int i = 0; i < 100; i++) {
+    DigiKeyboard.print("I love you");
+    DigiKeyboard.println(); // Send a newline character instead of Enter key
+    DigiKeyboard.delay(100);
+  }
 
-// Close Notepad
-DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-DigiKeyboard.delay(500);
-DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-DigiKeyboard.delay(500);
+  // Close Notepad
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
 
-// Turn on the LED for 5 seconds and then turn it off
-digitalWrite(1, HIGH);
-DigiKeyboard.delay(5000);
-digitalWrite(1, LOW);
-DigiKeyboard.delay(5000);
+  // Turn on the LED for 5 seconds and then turn it off
+  digitalWrite(1, HIGH);
+  DigiKeyboard.delay(5000);
+  digitalWrite(1, LOW);
+  DigiKeyboard.delay(5000);
 }
